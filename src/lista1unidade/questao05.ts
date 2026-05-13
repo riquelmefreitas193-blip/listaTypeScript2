d27241419642ea175f8b9f14a713aca5ad91b8cb
wendy-dev-byte <mileachristibarbosa@gmail.com>
2026-05-13 10:26:21 -0300

    modularizacao
    aula top

---
+++ src/lista1unidade/questao05.ts
@@ -0,0 +1,2 @@
+// 5- Faça um algoritmo que receba dois números e ao final mostre a soma, subtração,
+// multiplicação e a divisão dos números lidos.a
---
+++ src/modularizacao/calculadora.ts
@@ -0,0 +1,19 @@
+function soma1 (x: number, y:number):any{
+    console.log(x + y)
+}
+function subtracao (x:number, y:number):any{
+    console.log(x - y)
+}
+function multiplicacao (x:number, y:number):any{
+    console.log(x * y)
+}
+function divisao (x:number, y:number):any{
+    console.log(x / y)
+}
+let x:number = Number(prompt("informe um número: "))
+let y:number = Number(prompt("Informe outro número: "))
+
+
+
+
+
